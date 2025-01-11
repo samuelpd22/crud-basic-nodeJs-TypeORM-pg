a5dbb44bcb082881cd5c8e8e1dbd9561cc45d83c
samuelpd22 <samuel050101@hotmail.com>
2025-01-11 16:31:28 -0300

    feat: create entities

---
+++ src/app/entities/User.ts
@@ -0,0 +1,17 @@
+
+import { Entity, Column , PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity('users')
+class User{
+
+    @PrimaryGeneratedColumn('increment')
+    id:number;
+
+    @Column( 'varchar', {length:100 , nullable:false})
+    name: string;
+
+    @Column( 'varchar', {length:100 , nullable:false})
+    email: string;
+    
+}
+export default User;
