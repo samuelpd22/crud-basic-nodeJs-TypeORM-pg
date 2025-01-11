fbf2dfa103f552faeda134ead0edeb2059918316
samuelpd22 <samuel050101@hotmail.com>
2025-01-11 16:31:53 -0300

    feat: config routes

---
+++ src/app/routers/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import userRouter from "../controllers/UserController";
+
+
+const routers = Router();
+
+routers.use('/users',userRouter);
+
+export default routers;
